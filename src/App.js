1f45b402799bb06f1e18e50b0a5be5dcd9051bee
Bruno Krebs <bruno.krebs@auth0.com>
2019-05-23 11:16:02 -0300

    configuring material ui and icons;

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import './App.css';
+import Button from '@material-ui/core/Button';
+import Info from '@material-ui/icons/Info';
 
 function App() {
-  return <div />;
+  return (
+    <div>
+      <Button variant="contained" color="secondary">
+        <Info /> Test
+      </Button>
+    </div>
+  );
 }
-
 export default App;
